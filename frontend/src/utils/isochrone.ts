4f5afb9338dd307cb21d8cdbfc8521ee609ea2d6
pierreleripoll <pierreripoll13@gmail.com>
2023-01-14 23:07:37 +0100

    feat(error): handle error from API v alert

--- frontend/src/utils/isochrone.ts
+++ frontend/src/utils/isochrone.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Feature, GeoJsonProperties, Geometry } from "geojson";
 import { ExtendedFeatureCollection } from "d3";
 
@@ -10,6 +10,18 @@ export const listTransportModes = [
   { name: "Wheelchair", profile: "wheelchair" },
   { name: "Public transport", profile: "public_transport" },
 ];
+
+type ErrorTravelTime = {
+  description: string;
+  documentation_link: string;
+  error_code: number;
+  http_status: number;
+};
+
+type ErrorORS = {
+  error: { code: number; message: string };
+  info: { engine: { version: string; build_date: string }; timestamp: number };
+};
 
 const getIsochroneORS = (
   location: [number, number],
@@ -24,9 +36,15 @@ const getIsochroneORS = (
       range: [time],
       range_type: "time",
     })
-    .then(
-      (res) => res.data.features[0] as Feature<Geometry, GeoJsonProperties>
-    );
+    .then((res) => res.data.features[0] as Feature<Geometry, GeoJsonProperties>)
+    .catch((error: AxiosError<ErrorORS>) => {
+      if (axios.isAxiosError(error)) {
+        // Access to config, request, and response
+        throw Object.assign(error, {
+          error_description: error.response?.data.error.message,
+        });
+      } else throw error;
+    });
 };
 
 const getIsochroneTravelTime = (
@@ -58,9 +76,15 @@ const getIsochroneTravelTime = (
         },
       }
     )
-    .then(
-      (res) => res.data.features[0] as Feature<Geometry, GeoJsonProperties>
-    ); // TravelTime API returns a FeatureCollection, we only want the first feature (the isochron
+    .then((res) => res.data.features[0] as Feature<Geometry, GeoJsonProperties>) // TravelTime API returns a FeatureCollection, we only want the first feature (the isochron
+    .catch((error: AxiosError<ErrorTravelTime>) => {
+      if (axios.isAxiosError(error)) {
+        // Access to config, request, and response
+        throw Object.assign(error, {
+          error_description: error.response?.data.description,
+        });
+      } else throw error;
+    });
 };
 
 export const getIsochrone = (
