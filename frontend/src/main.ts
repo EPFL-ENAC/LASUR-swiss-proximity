5b07c42121c5a8186b36b24acdbc7053bbdd4ef6
pierreleripoll <pierreripoll13@gmail.com>
2023-02-06 00:12:49 +0100

    feat(ux): add style & font from alicelanding #40

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,17 +4,22 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+import "./styles/main.css";
+import "./styles/alice-font.scss";
+import "./styles/alicelanding.scss";
+// import "./styles/variables.scss";
+
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
-const app = createApp(App)
+const app = createApp(App);
 
-registerPlugins(app)
+registerPlugins(app);
 
-app.mount('#app')
+app.mount("#app");
