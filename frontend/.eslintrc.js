a80b76b9dc6b08b5cea103fd6a85aad35c560cfd
pierreleripoll <pierreripoll13@gmail.com>
2023-02-21 00:32:54 +0100

    fix(config): added back rules for no console/debugger

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,4 +12,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
+  },
 };
