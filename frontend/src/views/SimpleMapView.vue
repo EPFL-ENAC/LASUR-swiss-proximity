7876ace552a6d4708f1423efb6476585111c8d33
pierreleripoll <pierreripoll13@gmail.com>
2023-10-08 21:46:07 +0200

    feat(map): add map type selector

--- frontend/src/views/SimpleMapView.vue
+++ frontend/src/views/SimpleMapView.vue
@@ -3,12 +3,8 @@
     class="d-flex fill-height pa-0 align-stretch flex-column flex-nowrap"
     fluid
   >
-    <v-row dense class="flex-grow-0 no-gutters">
+    <v-row class="flex-grow-0 no-gutters">
       <v-col>
-        <v-card flat>
-          <v-card-title>Valeurs de l'étude </v-card-title>
-          <v-card-text> </v-card-text>
-        </v-card>
         <v-divider></v-divider>
       </v-col>
     </v-row>
@@ -45,6 +41,14 @@
           :distance="selectedDistance"
           :has-geocoder-search="true"
         ></VectorsMap>
+
+        <v-switch
+          v-model="isDemand"
+          class="overmap-switch"
+          hide-details
+          inset
+          :label="`${isDemand ? 'Demande' : 'Offre'}`"
+        ></v-switch>
       </v-col>
     </v-row>
   </v-container>
@@ -62,22 +66,42 @@ import {
   listYears,
   proximityTripColors,
 } from "@/utils/variables";
-import type { TileParams, ProximityVariable } from "@/utils/variables";
+import type { TileParams, ProximityVariable, MapType } from "@/utils/variables";
 import DemandConfigColumn from "@/components/DemandConfigColumn.vue";
+
+const storageMapTypeSource = "selectedMapType",
+  storageItemMapType = sessionStorage.getItem(storageMapTypeSource),
+  savedMapType: MapType = storageItemMapType
+    ? (storageItemMapType as MapType)
+    : "demand";
+
+const selectedMapType = ref(savedMapType);
+const isDemand = computed({
+  get() {
+    return selectedMapType.value === "demand";
+  },
+  set(value) {
+    selectedMapType.value = value ? "demand" : "supply";
+  },
+});
 
 const listVariables = listPossibleVariablesDemand;
 
-const isDemand = computed(() => {
-  return selectedTilesSource.value.name.includes("demand");
-});
-
 const defaultVariables: ProximityVariable[] = listVariables.map(
-  ({ id, name }) => ({
-    id,
-    name,
-    weight: 1,
-    selected: true,
-  })
+  ({ id, name }) =>
+    isDemand
+      ? {
+          id,
+          name,
+          weight: 1,
+          selected: id == "All_modes",
+        }
+      : {
+          id,
+          name,
+          weight: 1,
+          selected: true,
+        }
 );
 
 const storageKeyVariables = "selectedVariables",
@@ -137,3 +161,17 @@ function resetSessionStorage() {
   selectedYear.value = listYears[0];
 }
 </script>
+
+<style scoped lang="scss">
+.overmap-switch {
+  position: absolute;
+  bottom: 1em;
+  padding: 0.3em 2em;
+  border-radius: 1em;
+  min-width: 12.2em;
+  left: 50vw;
+  z-index: 1000;
+  background-color: white;
+  outline: solid 2px;
+}
+</style>
