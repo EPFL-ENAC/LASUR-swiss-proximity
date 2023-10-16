6fa269fe479a568be5745b8d6e63857fbf894b74
pierreleripoll <pierreripoll13@gmail.com>
2023-10-16 22:57:41 +0200

    refactor(map): refactor loading circle

---
+++ frontend/src/components/LoadingCircle.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-progress-circular
+    v-if="loading"
+    :size="50"
+    :width="5"
+    class="progress-circular"
+    color="white"
+    indeterminate
+  ></v-progress-circular>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  loading: boolean;
+}>();
+</script>
+
+<style scoped>
+.progress-circular {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+}
+</style>
